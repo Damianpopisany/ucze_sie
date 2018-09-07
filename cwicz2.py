aa41154a5e9d69b33d7e248b72e35e31397326da
unknown <damianpruskiewicz@gmail.com>
2018-09-07 15:13:12 +0200

    moj pierwszy commit

---
+++ cwicz2.py
@@ -0,0 +1,15 @@
+import math
+
+signal_power = 50
+noise_power = 10
+#print(math)
+ratio = (signal_power/noise_power)
+print(ratio)
+
+decibels = 10 * math.log10(ratio)
+print(decibels)
+
+radians = 0.7
+height = math.sin(radians)
+print(height)
+print(height*radians*noise_power)
