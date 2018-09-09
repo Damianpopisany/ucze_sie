211591302f876c6ceb65be1de9e2bea99d18d09a
Damianpopisany <damainpruskiewicz@gmail.com>
2018-09-09 16:20:23 +0200

    09.09.2018 version 1

--- cwicz.py
+++ cwicz.py
@@ -1,8 +1,8 @@
 import math
 
-signal_power = 50
-noise_power = 10
-#print(math)
+signal_power = 20
+noise_power = 123
+print(math)
 ratio = (signal_power/noise_power)
 print(ratio)
 
@@ -11,5 +11,12 @@ print(decibels)
 
 radians = 0.7
 height = math.sin(radians)
-print(height)
-print(height*radians*noise_power)
+
+
+degrees = 45
+radians =degrees / 180.0 * math.pi
+math.sin(radians)
+
+math.sqrt(2) / 2
+
+x = math.sin(degrees / 360.0 * 2 * math.pi)
--- cwicz2.py
+++ cwicz2.py
@@ -1,15 +1,18 @@
+
 import math
 
-signal_power = 50
-noise_power = 10
-#print(math)
-ratio = (signal_power/noise_power)
-print(ratio)
+degrees = 10224242
+x = 32443224242442
 
-decibels = 10 * math.log10(ratio)
-print(decibels)
+x = math.sin(degrees / 360.0 * 2 * math.pi)
+x = math.exp(math.log(344333443 + 1))
 
-radians = 0.7
-height = math.sin(radians)
-print(height)
-print(height*radians*radians+4*noise_power)
+
+def print_lyrics:
+
+print (print_lyrics at blabla  )
+type (print_lyrics)
+
+
+
+
