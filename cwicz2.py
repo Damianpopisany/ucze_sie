b27222d296779a3ad412375dd0fea18f3625de21
Damianpopisany <damainpruskiewicz@gmail.com>
2018-09-10 16:04:51 +0200

    09.09.2018 version 1

--- cwicz2.py
+++ cwicz2.py
@@ -8,10 +8,26 @@ x = math.sin(degrees / 360.0 * 2 * math.pi)
 x = math.exp(math.log(344333443 + 1))
 
 
-def print_lyrics:
+def print_lyrics():
+    print("Jestem drwalem i dobrze sie z tym czuję.",)
+    print("łukasz dzieki za pomoc")
+print_lyrics()
 
-print (print_lyrics at blabla  )
-type (print_lyrics)
+def print_lyrics():
+    print("leeeeee")
+    print("beeeeee")
+print_lyrics()
+
+def repeate_lyrics():
+    print_lyrics()
+    print_lyrics()
+repeate_lyrics()
+
+
+
+
+
+
 
 
 
